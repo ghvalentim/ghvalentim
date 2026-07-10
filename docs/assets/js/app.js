43013c65064b3e2766a7fba5ec464a84f736c2a0
Gabriel Valentim Carvalho <gabrielvalentinc@hotmail.com>
2026-07-10 16:50:55 +0100

    mais uma vez

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -4,10 +4,8 @@ import { fetchGitHubProjects } from './github.js';
 
 // Inicialização centralizada
 document.addEventListener('DOMContentLoaded', () => {
-  // O código aqui orquestra a chamada de cada módulo
-  // sem precisar de múltiplas tags script no HTML
   const preferredLanguage = localStorage.getItem('lang') || navigator.language.split('-')[0] || 'en';
-  initTheme();
   loadLanguage(preferredLanguage);
+  initTheme();
   fetchGitHubProjects();
 });
