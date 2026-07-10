1a1e2c67c839a70389e0fa4682f3df753411a78d
Gabriel Valentim Carvalho <gabrielvalentinc@hotmail.com>
2026-07-10 20:48:46 +0100

    10-07

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,11 +1,18 @@
 import { loadLanguage } from './i18n.js';
 import { initTheme } from './theme.js';
 import { fetchGitHubProjects } from './github.js';
+import { loadProjects } from './projects.js';
+import { loadSkills } from './skills.js';
+import { initMeta } from './utils/config-loader.js';
 
 // Inicialização centralizada
 document.addEventListener('DOMContentLoaded', () => {
   const preferredLanguage = localStorage.getItem('lang') || navigator.language.split('-')[0] || 'en';
   loadLanguage(preferredLanguage);
-  initTheme();
   fetchGitHubProjects();
+  loadSkills();
+  loadProjects();
+  initTheme();
+  initMeta();
+  
 });
